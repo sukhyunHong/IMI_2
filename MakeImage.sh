afc92f8130dda55e7784a5d81cd10772c8e9adae
Allist <ccma750@gmail.com>
2021-11-15 15:16:55 +0900

    in my case, when I use defconfig, fail to make image. so comment first line.

--- MakeImage.sh
+++ MakeImage.sh
@@ -1,5 +1,5 @@
 cd linux-5.4
 
 
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs -j40
+#make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs -j 8
